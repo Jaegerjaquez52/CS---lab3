6289feb99c1857a44e17b04070a09b8c9e006dd0
Jaegerjaquez52 <andriy007polischuk@gmail.com>
2025-12-24 08:11:37 +0200

    Update AppUserRepository.java

--- src/main/java/com/example/demo/repository/AppUserRepository.java
+++ src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.AppUser;
+// Бібліотека Spring Data JPA (Це головний інтерфейс, який дає методи save, findAll тощо)
 import org.springframework.data.jpa.repository.JpaRepository;
+// Стандартна бібліотека Java 
 import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
